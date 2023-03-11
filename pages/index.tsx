a0529598b55adc493269b1db80b0ef638d99c27b
Liko <likui2054@gmail.com>
2023-03-12 00:33:35 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,11 +104,15 @@ const Home: NextPage = () => {
         </Card>
         <div style={{fontSize: '14px', textAlign: 'left', textDecoration: 'underline'}}>
           <a href="https://xiaobot.net/p/aiyanjiu?refer=a1be91e6-ff56-473b-8404-a6f1f5856dba" target="_blank" rel="noopener noreferrer">
-          AI项目商业分析
+          AI项目商业分析 
           </a>
           <a href="https://xiaobot.net/p/MJ2023?refer=a1be91e6-ff56-473b-8404-a6f1f5856dba" target="_blank" rel="noopener noreferrer">
-          | Midjourney进阶创意库
+          |  Midjourney进阶创意库
           </a>
+          <a href="https://okjk.co/Ea2Pnf" target="_blank" rel="noopener noreferrer">
+          |  即刻@Liko
+          </a>
+          
         </div>
         
 
